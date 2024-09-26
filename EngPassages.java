ccdc33001e229509070fe3b0664759edc09d4c1a
Akhi Azad <akhiazad36@gmail.com>
2024-09-27 04:54:22 +0600

    second commit

--- EngPassages.java
+++ EngPassages.java
@@ -1,10 +1,10 @@
 public class EngPassages {
     private static final String[] passages = {
-        "You're a little scary sometimes, you know that? Brilliant... but scary.",
-        "Love as powerful as your mother’s for you leaves its own mark.",
-        "I hope you're pleased with yourselves. We could all have been killed.",
-        "When you play the game of thrones, you win or you die.",
-        "Do not pity the dead, Harry. Pity the living, and above all, those who live without love."
+        "You're a little scary sometimes, you know that? Brilliant... but scary. Do not pity the dead, Harry. Pity the living, and above all, those who live without love.",
+        "A Hare who ridicules a slow-moving Tortoise. Tired of the Hare's arrogant behaviour, the Tortoise challenges him to a race. The hare soon leaves the tortoise behind and, confident of winning, takes a nap midway through the race.",
+        "Driven by hunger, a fox tried to reach some grapes hanging high on the vine but was unable to, although he leaped with all his strength. As he went away, the fox remarked 'Oh, you aren't even ripe yet! I don't need any sour grapes.",
+        "A grasshopper that has spent the summer singing and dancing while the ant worked to store up food for winter. When winter arrives, the grasshopper finds itself dying of hunger and begs the ant for food. However, the ant rebukes its idleness and tells it to dance the winter away now.",
+        "A group of mice agree to attach a bell to a cat's neck to warn of its approach in the future, but they fail to find a volunteer to perform the job."
     };
 
     public static String[] getPassages() {
